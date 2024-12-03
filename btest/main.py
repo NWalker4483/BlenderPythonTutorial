9ae251ff2981b4e0ac984300aad94582a2344c86
Nile Walker <nilezwalker@gmail.com>
2024-12-03 07:00:06 -0500

    Update main.py

--- btest/main.py
+++ btest/main.py
@@ -2,12 +2,16 @@ from .make_circle import create_cubes_in_circle
 from .menu import register
 
 def main():
-    # # Example usage - change these values as needed
-    # NUM_CUBES = 35    # Number of cubes
-    # RADIUS = 5.0     # Circle radius
-    # CUBE_SIZE = 0.5  # Size of each cube
-
-    # Create the cubes
-
-    #create_cubes_in_circle(NUM_CUBES, RADIUS, CUBE_SIZE)
-    register()
+    stage=1
+    if stage == 1:
+        # Example usage - change these values as needed
+        NUM_CUBES = 35    # Number of cubes
+        RADIUS = 5.0     # Circle radius
+        CUBE_SIZE = 0.5  # Size of each cube
+    
+        # Create the cubes
+    
+        create_cubes_in_circle(NUM_CUBES, RADIUS, CUBE_SIZE)
+        
+    if stage == 2:
+        register()
